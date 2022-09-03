5b5b831ffaef7af4f320d746147bcf144b62d56b
rabiul155 <siyamict@gmail.com>
2022-09-03 14:25:30 +0600

    news loaded

--- app.js
+++ app.js
@@ -7,25 +7,44 @@ const allNews = async () => {
     const url = 'https://openapi.programming-hero.com/api/news/categories';
     const res = await fetch(url);
     const data = await res.json();
-    displayNews(data.data.news_category);
+    loadNews(data.data.news_category);
 }
 
-const displayNews = (data) => {
-    console.log(data);
+const loadNews = (data) => {
+    // console.log(data);
     const newsButton = document.getElementById('news-button');
     // console.log(newsButton);
-
-
-    for (const news of data) {
+    data.forEach(news => {
         const div = document.createElement('div');
         div.classList.add('d-flex');
         div.classList.add('justify-content-between')
-
         div.innerHTML = `
-        <button id="${news.category_id} " type="button" class="border-0 btn btn-outline-secondary">${news.category_name}</button>
+        <button onclick="loadNewsCategory('${news.category_id}')" type="button" class="border-0 btn btn-outline-secondary">${news.category_name}</button>
         `;
         newsButton.appendChild(div);
-    }
+    });
 
 }
+
+// news category 
+
+const loadNewsCategory = async (id) => {
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsCategory(data.data);
+
+
+}
+
+const displayNewsCategory = (data) => {
+    console.log(data);
+
+
+
+
+}
+
+
 allNews();
